0fec9d13f21c756628e567f9f43a748eb8af56bb
Shivam Rajvir <shivamrajvir@gmail.com>
2023-09-21 16:20:40 +0530

    removed setTimeout, was trying to see if it works for images

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -1,14 +1,12 @@
 const githubReducer = (state, action) => {
     switch (action.type) {
         case 'GET_USERS':
-            setTimeout(() => {
                 return {
                     ...state,
                     users: action.payload,
                     loading: false
                 }
-                
-            })
+
         default:
             return state;
     }
